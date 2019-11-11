cf5ad49cb5f74df71a17e13db5aa513510548e28
Avik Chakraborty <cavik1994@gmail.com>
2019-11-11 17:58:40 +0530

    Add files via upload

--- content.js
+++ content.js
@@ -2,7 +2,12 @@ $(function(){
     var keyword = "videos";
     var pageTitle = document.title.trim();
     var metaElements = document.getElementsByTagName('meta');
-    for(let i = 0; i < metaElements.length; i++)
+    let regexTestPageTitle = new RegExp('\\b' + keyword + '\\b').test(pageTitle);
+    if(regexTestPageTitle){
+        window.location.href = "https://www.google.com/"
+    }
+    else{
+        for(let i = 0; i < metaElements.length; i++)
     {
         if(metaElements[i].getAttribute('name') == "description" || metaElements[i].getAttribute('name') == "Description"){
             metaDesc = metaElements[i].getAttribute('content');
@@ -12,14 +17,15 @@ $(function(){
             
         }
     }
+    }
+    
 
     function checkKeyWord(metaDesc){
 
         let regexTestMetaDesc = new RegExp('\\b' + keyword + '\\b').test(metaDesc);
-        let regexTestPageTitle = new RegExp('\\b' + keyword + '\\b').test(pageTitle);
-
-        if(regexTestMetaDesc == true ||  regexTestPageTitle == true){
-           window.location.href = "https://www.google.com/"
+     
+        if(regexTestMetaDesc == true){
+         
            
         }
     }
